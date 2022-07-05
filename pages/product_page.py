fc55e685bb829cd2a9848f3f65da521590ba55f0
Евгений <fandeev012@gmail.com>
2022-07-05 11:46:50 +0300

    Добавлены тесты test_guest_cant_see_success_message_after_adding_product_to_basket, test_guest_cant_see_success_message, test_message_disappeared_after_adding_product_to_basket. Также для этих тестов были добавлены методы в класс ProductPage: should_not_be_success_message и should_not_be_success_message_disappeared.

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,7 +7,6 @@ class ProductPage(BasePage):
         add_product_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_product_to_basket_button.click()
 
-
     def should_be_add_product_to_basket_button(self):
         assert self.is_element_present(
             *ProductPageLocators.ADD_TO_BASKET_BUTTON), "Add to basket button is not presented"
@@ -32,7 +31,6 @@ class ProductPage(BasePage):
     def get_basket_product_price(self):
         return self.browser.find_element(*ProductPageLocators.BASKET).text.split()[2]
 
-
     def should_be_product_price_equal_to_product_price_in_basket(self):
         print(self.get_basket_product_price())
         assert self.get_product_price() == self.get_basket_product_price() \
@@ -41,4 +39,12 @@ class ProductPage(BasePage):
     def should_be_product_name_in_basket_success_alerts(self):
         print(self.get_basket_success_alert())
         assert self.get_product_name() == self.get_basket_success_alert() \
-               ,"Product not added to basket"
+            , "Product not added to basket"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.BASKET_SUCCESS_ALERTS), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_success_message_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.BASKET_SUCCESS_ALERTS), \
+            "Success message is presented, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,8 @@
-import time
 
 import pytest
 
 from .pages.product_page import ProductPage
-
+from .pages.locators import ProductPageLocators
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -12,7 +11,9 @@ from .pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  pytest.param(
+                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                      marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
@@ -26,3 +27,23 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.should_be_product_price_equal_to_product_price_in_basket()
     product_page.should_be_product_name_in_basket_success_alerts()
 
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"])
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.click_on_add_product_to_basket_button()
+    product_page.should_not_be_success_message()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"])
+def test_guest_cant_see_success_message(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"])
+def test_message_disappeared_after_adding_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.click_on_add_product_to_basket_button()
+    product_page.should_not_be_success_message_disappeared()
